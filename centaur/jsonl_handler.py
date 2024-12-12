9178c0c7eb4a5fa01dadf6fff07aec0574bbfd6f
Can Mingir <canmingir@gmail.com>
2024-12-12 10:28:35 -0500

    Schedule JSONL with given id

--- centaur/jsonl_handler.py
+++ centaur/jsonl_handler.py
@@ -1,27 +1,22 @@
 from watchdog.events import FileSystemEventHandler
 import json
-from centaur.session import clients
+
 
 class JSONLHandler(FileSystemEventHandler):
-    def __init__(self, file_id):
+    def __init__(self, file_id, queue):
         self.file_id = file_id
+        self.queue = queue
+        self.last_read_position = 0
 
     def on_modified(self, event):
         if event.src_path.endswith(self.file_id + ".jsonl"):
             with open(event.src_path, 'r', encoding='utf-8') as f:
-                for line in f:
+                f.seek(self.last_read_position)
+                while line := f.readline():
                     try:
                         data = json.loads(line)
-                        if data["type"] == "data":
-                            if self.file_id in clients:
-                                for client in clients[self.file_id]:
-                                    client.queue.put(f"data: {json.dumps(data)}\n\n")
-                        elif data["type"] == "system" and data["event"] == "stream_end":
-                            if self.file_id in clients:
-                                for client in clients[self.file_id]:
-                                    client.queue.put("event: stream_end\n\n")
-                                    client.close()
-                                del clients[self.file_id]
-                                break
+                        self.queue.put(data)
                     except json.JSONDecodeError:
-                        pass
+                        continue
+                self.last_read_position = f.tell()
+
--- centaur/routes/evaluate.py
+++ centaur/routes/evaluate.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, Response
+from flask import Blueprint, Response, jsonify
 from watchdog.observers import Observer
 from queue import Queue
 import os
 import time
+import json
 import tempfile
 from centaur.session import clients
 from centaur.jsonl_handler import JSONLHandler
@@ -13,28 +14,32 @@ blueprint = Blueprint("evaluate", __name__)
 @blueprint.route('/evaluate/<id>')
 def evaluate(id):
     def stream():
-        queue = Queue()
-
         file_path = os.path.join(tempfile.gettempdir(), "centaur", f"{id}.jsonl")
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
         with open(file_path, "w") as file:
             pass
 
-        if id not in clients:
-            clients[id] = []
+            queue = Queue()
             observer = Observer()
-            observer.schedule(JSONLHandler(id), os.path.join(tempfile.gettempdir(), "centaur"), recursive=False)
+            observer.schedule(JSONLHandler(id, queue), os.path.join(tempfile.gettempdir(), "centaur"), recursive=False)
             observer.start()
-        clients[id].append(queue)
-        try:
-            while True:
-                data = queue.get()
-                yield data
-                time.sleep(0.1)
-        except GeneratorExit:
-            clients[id].remove(queue)
-            if not clients[id]:
-                del clients[id]
 
-    return Response(stream(), mimetype='text/event-stream')
+            try:
+                while True:
+                    if not queue.empty():
+                        data = queue.get()
+                        if data["type"] == "system" and data["event"] == "stream_end":
+                            observer.stop()
+                            break
+                        else:
+                            yield f"data: {json.dumps(data)}\n\n"
+                    time.sleep(0.1)
+            except GeneratorExit:
+                observer.stop()
+
+    if id not in clients:
+        clients[id] = True
+        return Response(stream(), mimetype='text/event-stream')
+    else:
+        return jsonify({"error": "already_processed"}), 400
