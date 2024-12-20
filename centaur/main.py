08eeda632716c33c06d4204de527055b1175c698
Can Mingir <canmingir@gmail.com>
2024-12-20 10:44:19 -0500

    Add CORS support

--- centaur/main.py
+++ centaur/main.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.evaluate import blueprint
 
 app = Flask(__name__)
+CORS(app)
 app.register_blueprint(blueprint)
 
 if __name__ == '__main__':
