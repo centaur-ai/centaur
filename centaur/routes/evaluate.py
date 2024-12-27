f8a78932f4deb59d0a59597434cdae15c30ea433
Can Mingir <canmingir@gmail.com>
2024-12-27 06:15:11 -0500

    Fire and forget PWL in subprocess

--- centaur/routes/evaluate.py
+++ centaur/routes/evaluate.py
@@ -43,7 +43,7 @@ def stream():
 
     if id not in clients:
         clients[id] = True
-        subprocess.run(f"{pwl_path}/pwl_reasoner_dbg {file} --id {id}", shell=True, cwd=pwl_path)
+        subprocess.Popen(f"{pwl_path}/pwl_reasoner_dbg {file} --id {id}", shell=True, cwd=pwl_path)
         return jsonify({"id": id}), 200
     else:
         return jsonify({"error": "already_processed"}), 400
