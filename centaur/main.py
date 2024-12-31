199744194246b1330b7b8fa71b208fae65951939
Can Mingir <canmingir@gmail.com>
2024-12-31 07:43:37 -0500

    Deprecate Poetry

--- centaur/main.py
+++ centaur/main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from routes.evaluate import blueprint
+from centaur.routes.evaluate import blueprint
 
 app = Flask(__name__)
 CORS(app)
--- poetry.run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-poetry run python centaur/main.py
