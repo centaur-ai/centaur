4a26cd76629124efdf1863b17571ce3de23a661d
Can Mingir <canmingir@gmail.com>
2025-01-13 05:20:52 -0500

    Separate query from description

--- centaur/routes/evaluate.py
+++ centaur/routes/evaluate.py
@@ -18,7 +18,12 @@ pwl_path = os.getenv("PWL_PATH", "/home/ubuntu/qa/PWL")
 @blueprint.route('/evaluate', methods=['POST'])
 def stream():
     data = request.get_json()
-    id = uuid.uuid4()
+    evaluate_id = uuid.uuid4()
+
+    if "query" in data:
+        query =  data["query"]
+    else:
+        query = None
 
     if "description" in data:
         description = data["description"]
@@ -28,23 +33,23 @@ def stream():
     if "file" in data:
         file = data["file"]
     else:
-        file = os.path.join(pwl_path, f"{id}.txt")
+        file = os.path.join(pwl_path, f"{evaluate_id}.txt")
         with open(file, "w") as f:
             f.write(description)
             f.close()
 
     stream_path = os.path.join(
-        tempfile.gettempdir(), "centaur", f"{id}.jsonl")
+        tempfile.gettempdir(), "centaur", f"{evaluate_id}.jsonl")
     os.makedirs(os.path.dirname(stream_path), exist_ok=True)
 
     with open(stream_path, "w") as f:
-        f.write(json.dumps({"type": "system", "event": "stream_start", "description": description}) + "\n")
+        f.write(json.dumps({"type": "system", "event": "stream_start", "description": description, "query": query}) + "\n")
         f.close()
 
-    if id not in clients:
-        clients[id] = True
+    if evaluate_id not in clients:
+        clients[evaluate_id] = True
         subprocess.Popen(f"{pwl_path}/stream.sh {file} {id}", shell=True, cwd=pwl_path)
-        return jsonify({"id": id}), 200
+        return jsonify({"id": evaluate_id}), 200
     else:
         return jsonify({"error": "already_processed"}), 400
 
